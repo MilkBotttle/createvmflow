0102eff8576589cd0a6aa45807c343f942f9a926
milkbotttle <cameron.c@inwinstack.com>
2018-03-06 16:09:37 +0800

    add login url

--- vmcreateflow/settings.py
+++ vmcreateflow/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL='/workflow'
